69f50ba7f7edb4ac90e8558bd34d7c78584ff083
Vapaa Eero <eero.hallikainen@gmail.com>
2024-10-09 14:00:27 +0300

    toka

--- index.js
+++ index.js
@@ -110,6 +110,7 @@ app.get('/info/', (request, response) => {
 })
     
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
